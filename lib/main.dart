df28aa6189e8b4b6abd8ec4acd1c8ebbfb7969b9
malavikaac <acmalavika@gmail.com>
2023-02-13 14:18:14 +0530

    IdCard

---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MaterialApp(
+      home: Card(),
+    ));
+
+class Card extends StatelessWidget {
+  const Card({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromARGB(255, 15, 15, 15),
+      appBar: AppBar(
+        title: Text(
+          'Id_Card',
+          style:
+              TextStyle(color: Color.fromARGB(255, 19, 18, 17), fontSize: 20.0),
+        ),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 135, 208, 218),
+        elevation: 0.0,
+      ),
+      body: Padding(
+          padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Center(
+                child:CircleAvatar(
+                  backgroundImage:AssetImage('assets/lap.jpg'),
+                  radius:50.0,
+                ),
+              ),
+            Text('Name',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 206, 233, 236),
+                  letterSpacing: 2.0,
+                )),
+                SizedBox(height: 10.0 ,),
+            Text('ABC',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 135, 208, 218),
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+                )
+                ),
+                SizedBox(height: 30.0 ,),
+                 Text('CURRENT LEVEL',
+                style: TextStyle(
+                  color:  Color.fromARGB(255, 206, 233, 236),
+                  letterSpacing: 2.0,
+                )),
+                SizedBox(height: 10.0 ,),
+            Text('3',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 135, 208, 218),
+                  letterSpacing: 2.0,
+                  fontSize: 28.0,
+                  fontWeight: FontWeight.bold
+                )
+                ),
+                 SizedBox(height: 30.0),
+                 Row(
+                  children: <Widget>[
+                    Icon(Icons.email,
+                    color:Color.fromARGB(255, 244, 245, 245),
+                    ),
+                    SizedBox(width:10.0),
+                   Text(
+                    'abcde@gmail.com',
+                style: TextStyle(
+                  color: Color.fromARGB(255, 135, 208, 218),
+                  letterSpacing: 1.0,
+                  fontSize: 20.0,
+                 
+                )
+                ), 
+                  ],
+                 )
+             ]
+          )
+          ),
+    );
+  }
+}
