9549882099253c31b892a8e0a7764d9224b38a7f
malavikaac <acmalavika@gmail.com>
2023-02-13 15:44:20 +0530

    IdCard

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,15 @@ void main() => runApp(const MaterialApp(
       home: Card(),
     ));
 
-class Card extends StatelessWidget {
+class Card extends StatefulWidget {
   const Card({super.key});
 
+  @override
+  State<Card> createState() => _CardState();
+}
+
+class _CardState extends State<Card> {
+  int level = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,73 +27,79 @@ class Card extends StatelessWidget {
         backgroundColor: Color.fromARGB(255, 135, 208, 218),
         elevation: 0.0,
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            level += 1;
+          });
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Color.fromARGB(255, 135, 208, 218),
+      ),
       body: Padding(
           padding: EdgeInsets.fromLTRB(30.0, 40.0, 30.0, 0.0),
           child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Center(
-                child:CircleAvatar(
-                  backgroundImage:AssetImage('assets/lap.jpg'),
-                  radius:50.0,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Center(
+                  child: CircleAvatar(
+                    backgroundImage: AssetImage('assets/lap.jpg'),
+                    radius: 50.0,
+                  ),
                 ),
-              ),
-              Divider( 
-                height:80.0,
-                color: Color.fromARGB(255, 135, 198, 223),
-              ),
-            Text('Name',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 206, 233, 236),
-                  letterSpacing: 2.0,
-                )),
-                SizedBox(height: 10.0 ,),
-            Text('ABCDE',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 135, 208, 218),
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold
-                )
+                Divider(
+                  height: 80.0,
+                  color: Color.fromARGB(255, 135, 198, 223),
                 ),
-                SizedBox(height: 30.0 ,),
-                 Text('CURRENT LEVEL',
-                style: TextStyle(
-                  color:  Color.fromARGB(255, 206, 233, 236),
-                  letterSpacing: 2.0,
-                )),
-                SizedBox(height: 10.0 ,),
-            Text('3',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 135, 208, 218),
-                  letterSpacing: 2.0,
-                  fontSize: 28.0,
-                  fontWeight: FontWeight.bold
-                )
+                Text('Name',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 206, 233, 236),
+                      letterSpacing: 2.0,
+                    )),
+                SizedBox(
+                  height: 10.0,
                 ),
-                 SizedBox(height: 20.0),
-                 Row(
+                Text('ABCDE',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 135, 208, 218),
+                        letterSpacing: 2.0,
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(
+                  height: 30.0,
+                ),
+                Text('CURRENT LEVEL',
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 206, 233, 236),
+                      letterSpacing: 2.0,
+                    )),
+                SizedBox(
+                  height: 10.0,
+                ),
+                Text('$level ',
+                    style: TextStyle(
+                        color: Color.fromARGB(255, 135, 208, 218),
+                        letterSpacing: 2.0,
+                        fontSize: 28.0,
+                        fontWeight: FontWeight.bold)),
+                SizedBox(height: 20.0),
+                Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Icon(Icons.email,
-                    color:Color.fromARGB(255, 244, 245, 245),
+                    Icon(
+                      Icons.email,
+                      color: Color.fromARGB(255, 244, 245, 245),
                     ),
-                    SizedBox(width:10.0),
-                   Text(
-                    'abcde@gmail.com',
-                style: TextStyle(
-                  color: Color.fromARGB(255, 135, 208, 218),
-                  letterSpacing: 1.0,
-                  fontSize: 20.0,
-                 
-                )
-                ), 
-
+                    SizedBox(width: 10.0),
+                    Text('abcde@gmail.com',
+                        style: TextStyle(
+                          color: Color.fromARGB(255, 135, 208, 218),
+                          letterSpacing: 1.0,
+                          fontSize: 20.0,
+                        )),
                   ],
-                 )
-               ]
-             )
-        ),
+                )
+              ])),
     );
   }
 }
